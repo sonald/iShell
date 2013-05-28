79cb68ecb6e52a8cfb835f4fd963475b0d70f771
Sian Cao <sycao@redflag-linux.com>
2013-05-28 10:34:45 +0800

    Initial Commit

---
+++ iShell/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iShell
+//
+//  Created by Sian Cao on 13-5-28.
+//  Copyright (c) 2013年 Freedom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
